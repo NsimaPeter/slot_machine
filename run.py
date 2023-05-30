4e8609bfb0836c76cfba14457a9ff7d3efa48165
Nsima Peter <peternsima@gmail.com>
2023-05-30 08:50:44 +0000

    add the play function, while loop and if/else statements and also add comment and start_game commit

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import random
 
 #This is the symbols and their respective weights
-symbols = ['Football', 'Bell', 'Lemon', 'Orange', 'Star', 'Skull']
+symbols = ['Football', 'Bell', 'Lemon', 'Orange', 'Star', 'Moon']
 weights = [0.4, 0.3, 0.2, 0.05, 0.03, 0.02]
 
 # This is Function to spin the slot machine
@@ -19,4 +19,23 @@ def check_win(result):
     if result[0] == result[1] == result[2]:
         return True
     return False
+
+
+# Function to play the game
+def play_game():
+    print('=== Welcome to the Text-Based Slot Machine ===')
+    while True:
+        input('Press Enter to spin the slot machine...')
+        result = spin()
+        display_result(result)
+        if check_win(result):
+            print('Congratulations! You won!')
+        else:
+            print('Sorry, you lost. Try again!')
+        choice = input('Do you want to play again? (y/n): ')
+        if choice.lower() != 'y':
+            break
+
+# Start the game
+play_game()
 
