859ea47b7a2bd9652f0260249fe7a8e2eabdba53
Nsima Peter <peternsima@gmail.com>
2023-05-30 08:10:36 +0000

    add functionthat checks if the result is a winning combination also add comments and commit changes

--- run.py
+++ run.py
@@ -1,16 +1,22 @@
 import random
 
-
-# the symbols and their respective weights
-symbols = ['Cherry', 'Bell', 'Lemon', 'Orange', 'Star', 'Skull']
+#This is the symbols and their respective weights
+symbols = ['Football', 'Bell', 'Lemon', 'Orange', 'Star', 'Skull']
 weights = [0.4, 0.3, 0.2, 0.05, 0.03, 0.02]
 
-# Function to spin the slot machine
+# This is Function to spin the slot machine
 def spin():
     # Generate three random symbols
     result = [random.choices(symbols, weights)[0] for _ in range(3)]
     return result
 
-# Function to display the result
+#This is Function to display the result
 def display_result(result):
     print(' | '.join(result))
+
+# This is Function to check if the result is a winning combination
+def check_win(result):
+    if result[0] == result[1] == result[2]:
+        return True
+    return False
+
